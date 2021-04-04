faaaa3c9cc741fee7ade6f914827df10f5ea6c3c
Reeve Jarvis <reevejarvis@hotmail.com>
2021-04-04 10:44:47 -0700

    Gallery Updates

--- gallery.js
+++ gallery.js
@@ -9,14 +9,14 @@ for (let i = 0; i < galleryImages.length; i++) {
   galleryImages[i].addEventListener("click", displayModalImage);
 }
 
+let span = document.getElementsByClassName("closeModal")[0];
+span.addEventListener("click", closeModalImage);
+
 function displayModalImage() {
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
-
-var span = document.getElementsByClassName("closeModal")[0];
-span.addEventListener("click", closeModalImage);
 
 function closeModalImage() {
   modal.style.display = "none";
